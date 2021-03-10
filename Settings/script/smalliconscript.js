2b60ae940984f604f3338ee04e0b0af4e89c2a11
shyamzzp <shyamsundersuthar009@gmail.com>
2021-03-10 11:38:17 +0530

    Add new files to the project.

---
+++ Settings/script/smalliconscript.js
@@ -0,0 +1,55 @@
+function addcustomer_smalldown() {
+    $("#addcustomersmall").attr("src", "images/smallicons/addcustomer_down.png");
+}
+
+function addcustomer_smallup() {
+    $("#addcustomersmall").attr("src", "images/smallicons/addcustomer_up.png");
+}
+
+function clearsmalldown() {
+    $("#clearsmall").attr("src", "images/smallicons/clear_down.png");
+}
+
+function clearsmallup() {
+    $("#clearsmall").attr("src", "images/smallicons/clear_up.png");
+}
+
+function updatesmalldown() {
+    $("#updatesmall").attr("src", "images/smallicons/updatecustomer_down.png");
+}
+
+function updatesmallup() {
+    $("#updatesmall").attr("src", "images/smallicons/updatecustomer_up.png");
+}
+
+function updateremarksmalldown() {
+    $("#updateremarksmall").attr("src", "images/smallicons/updateremark_down.png");
+}
+
+function updateremarksmallup() {
+    $("#updateremarksmall").attr("src", "images/smallicons/updateremark_up.png");
+}
+
+function changepasswordsmalldown() {
+    $("#changepasswordsmall").attr("src", "images/smallicons/changepassword_down.png");
+}
+
+function changepasswordsmallup() {
+    $("#changepasswordsmall").attr("src", "images/smallicons/changepassword_up.png");
+}
+
+function closesmalldown(id) {
+    $(id).attr("src", "images/smallicons/close_down.png");
+}
+
+function closesmallup(id) {
+    $(id).attr("src", "images/smallicons/close_up.png");
+}
+
+function submitsmalldown(id) {
+    $(id).attr("src", "images/smallicons/submit_down.png");
+}
+
+function submitsmallup(id) {
+    $(id).attr("src", "images/smallicons/submit_up.png");
+}
